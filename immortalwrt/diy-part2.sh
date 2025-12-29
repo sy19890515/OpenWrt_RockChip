b1590020cd1100825f51fbe32e8a2982127db8ca
y29shi <sy19890515@hotmail.com>
2025-12-30 02:30:22 +0800

    change gateway address to 192.168.4.1

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -2,4 +2,4 @@
 #
 # Modify default IP
 sed -i 's/192.168.4.1/192.168.11.1/g' package/base-files/files/bin/config_generate
-sed -i "s/ImmortalWrt/OpenWrt/g" package/base-files/files/bin/config_generate
+# sed -i "s/ImmortalWrt/OpenWrt/g" package/base-files/files/bin/config_generate
